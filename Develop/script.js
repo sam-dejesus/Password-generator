1056f670b678dac31b7e0d66bc5eaebf8e36f190
samuel de jesus <samue@MacBook-Air.local>
2023-03-07 13:35:25 -0500

    added functions if statements and variables

--- Develop/script.js
+++ Develop/script.js
@@ -15,17 +15,30 @@
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-var chars = "abcdefghijklmnopqrstuvwxyz";
-var specialChars = "!@#$%^&*()";
-var numChars = "0123456789";
-var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var chars = ["a","b","c","d","e","f","g","h",'i',"j",'k',"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var specialChars = ["!","@","#","$","%","^","&","*","(",")"];
+var numChars = [0,1,2,3,4,5,6,7,8,9]
+var upperChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+
 function password(){
-  if(window.confirm("want special charachters")){
-// adds the specialchar into the equation and continues the process
-window.confirm("want upper case chars")
-  }
-else {window.confirm("want upper case chars")}
+var getspecial = window.confirm("want special charachters");
+if(getspecial === true){
+  ;
 }
+var getNum = window.confirm("want numbers");
+if(getNum === true){
+  ;
+}
+var getupper = window.confirm("want upper case");
+if(getupper === true){
+  ;
+}
+var passLength = window.prompt("how many characters");
+if(getspecial === true){
+  ;
+}
+}
+  
 
 function start(){
   if( window.confirm("need a new password")){
